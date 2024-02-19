e21e5bcc3a2090d962ff5ea7ea4b5301d8b1939f
Ahmed Yousry Helal <102818092+AhmedYousry10@users.noreply.github.com>
2024-02-19 20:36:58 +0200

    updating admin form

---
+++ Attendance System Project/Attendance Management system/Attendance Management system/forms/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Attendance_Management_system
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panelBack = new Panel();
+            panelExpand = new Panel();
+            buttonMinimize = new Button();
+            panel3 = new Panel();
+            pictureBoxExpand = new PictureBox();
+            pictureBox2 = new PictureBox();
+            labelTime = new Label();
+            panelTop = new Panel();
+            labelRole = new Label();
+            label7 = new Label();
+            labelUsername = new Label();
+            label4 = new Label();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panelBack.SuspendLayout();
+            panelExpand.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxExpand).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panelTop.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(67, 3, 125);
+            panel1.Controls.Add(panel2);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(258, 681);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(258, 180);
+            panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(97, 125);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 19);
+            label2.TabIndex = 2;
+            label2.Text = "System";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(23, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(212, 19);
+            label1.TabIndex = 1;
+            label1.Text = "Attendance Management";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(81, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(93, 87);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panelBack
+            // 
+            panelBack.Controls.Add(panelExpand);
+            panelBack.Controls.Add(pictureBoxExpand);
+            panelBack.Controls.Add(pictureBox2);
+            panelBack.Controls.Add(labelTime);
+            panelBack.Controls.Add(panelTop);
+            panelBack.Dock = DockStyle.Top;
+            panelBack.Location = new Point(258, 0);
+            panelBack.Name = "panelBack";
+            panelBack.Size = new Size(926, 165);
+            panelBack.TabIndex = 3;
+            // 
+            // panelExpand
+            // 
+            panelExpand.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            panelExpand.BackColor = Color.FromArgb(67, 3, 125);
+            panelExpand.BorderStyle = BorderStyle.FixedSingle;
+            panelExpand.Controls.Add(button2);
+            panelExpand.Controls.Add(buttonMinimize);
+            panelExpand.Controls.Add(panel3);
+            panelExpand.Location = new Point(720, 58);
+            panelExpand.Name = "panelExpand";
+            panelExpand.Size = new Size(200, 107);
+            panelExpand.TabIndex = 8;
+            // 
+            // buttonMinimize
+            // 
+            buttonMinimize.BackColor = Color.White;
+            buttonMinimize.Cursor = Cursors.Hand;
+            buttonMinimize.Dock = DockStyle.Top;
+            buttonMinimize.FlatAppearance.BorderSize = 0;
+            buttonMinimize.FlatStyle = FlatStyle.Flat;
+            buttonMinimize.Font = new Font("Century Gothic", 9F);
+            buttonMinimize.ForeColor = Color.FromArgb(67, 3, 125);
+            buttonMinimize.Location = new Point(0, 5);
+            buttonMinimize.Name = "buttonMinimize";
+            buttonMinimize.Size = new Size(198, 48);
+            buttonMinimize.TabIndex = 1;
+            buttonMinimize.Text = "Minimize";
+            buttonMinimize.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(0, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(198, 5);
+            panel3.TabIndex = 0;
+            // 
+            // pictureBoxExpand
+            // 
+            pictureBoxExpand.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBoxExpand.Cursor = Cursors.Hand;
+            pictureBoxExpand.Image = (Image)resources.GetObject("pictureBoxExpand.Image");
+            pictureBoxExpand.Location = new Point(796, 34);
+            pictureBoxExpand.Name = "pictureBoxExpand";
+            pictureBoxExpand.Size = new Size(27, 24);
+            pictureBoxExpand.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxExpand.TabIndex = 9;
+            pictureBoxExpand.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(730, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(60, 58);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // labelTime
+            // 
+            labelTime.AutoSize = true;
+            labelTime.BackColor = Color.White;
+            labelTime.ForeColor = Color.FromArgb(67, 3, 125);
+            labelTime.Location = new Point(24, 23);
+            labelTime.Name = "labelTime";
+            labelTime.Size = new Size(28, 19);
+            labelTime.TabIndex = 3;
+            labelTime.Text = "{?}";
+            // 
+            // panelTop
+            // 
+            panelTop.BackColor = Color.FromArgb(67, 3, 125);
+            panelTop.Controls.Add(labelRole);
+            panelTop.Controls.Add(label7);
+            panelTop.Controls.Add(labelUsername);
+            panelTop.Controls.Add(label4);
+            panelTop.Dock = DockStyle.Bottom;
+            panelTop.Location = new Point(0, 65);
+            panelTop.Name = "panelTop";
+            panelTop.Size = new Size(926, 100);
+            panelTop.TabIndex = 0;
+            // 
+            // labelRole
+            // 
+            labelRole.AutoSize = true;
+            labelRole.BackColor = Color.FromArgb(67, 3, 125);
+            labelRole.ForeColor = Color.White;
+            labelRole.Location = new Point(154, 57);
+            labelRole.Name = "labelRole";
+            labelRole.Size = new Size(28, 19);
+            labelRole.TabIndex = 7;
+            labelRole.Text = "{?}";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.FromArgb(67, 3, 125);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(50, 57);
+            label7.Name = "label7";
+            label7.Size = new Size(46, 19);
+            label7.TabIndex = 6;
+            label7.Text = "Role:";
+            // 
+            // labelUsername
+            // 
+            labelUsername.AutoSize = true;
+            labelUsername.BackColor = Color.FromArgb(67, 3, 125);
+            labelUsername.ForeColor = Color.White;
+            labelUsername.Location = new Point(154, 22);
+            labelUsername.Name = "labelUsername";
+            labelUsername.Size = new Size(28, 19);
+            labelUsername.TabIndex = 5;
+            labelUsername.Text = "{?}";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.FromArgb(67, 3, 125);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(50, 22);
+            label4.Name = "label4";
+            label4.Size = new Size(87, 19);
+            label4.TabIndex = 4;
+            label4.Text = "Welcome:";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.Cursor = Cursors.Hand;
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Century Gothic", 9F, FontStyle.Bold);
+            button2.ForeColor = Color.FromArgb(67, 3, 125);
+            button2.Location = new Point(0, 53);
+            button2.Name = "button2";
+            button2.Size = new Size(198, 48);
+            button2.TabIndex = 2;
+            button2.Text = "Log Out";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1184, 681);
+            Controls.Add(panelBack);
+            Controls.Add(panel1);
+            Font = new Font("Century Gothic", 12F, FontStyle.Bold);
+            Margin = new Padding(4);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Attendance Management System";
+            WindowState = FormWindowState.Maximized;
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panelBack.ResumeLayout(false);
+            panelBack.PerformLayout();
+            panelExpand.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBoxExpand).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panelTop.ResumeLayout(false);
+            panelTop.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Panel panelBack;
+        private Label labelTime;
+        private Panel panelTop;
+        private Label label4;
+        private Label labelRole;
+        private Label label7;
+        private Label labelUsername;
+        private PictureBox pictureBoxExpand;
+        private PictureBox pictureBox2;
+        private Panel panelExpand;
+        private Button buttonMinimize;
+        private Panel panel3;
+        private Button button2;
+    }
+}
