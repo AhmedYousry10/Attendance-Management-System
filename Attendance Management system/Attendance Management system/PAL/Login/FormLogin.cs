53cb61aa1136714207770f1fb6bbf76245d89cc8
sherifebrahim <sherifebrahim440@gmail.com>
2024-02-28 02:54:39 +0200

    new version

--- Attendance Management system/Attendance Management system/PAL/Login/FormLogin.cs
+++ Attendance Management system/Attendance Management system/PAL/Login/FormLogin.cs
@@ -129,7 +129,7 @@ namespace AttendanceManagement.Attendance.Forms
 
         private void OpenDashboardForm(string username, string role)
         {
-            //not complited
+            //not complited yet
             FormMain dashboard = new FormMain();
             dashboard.Username = username;
             dashboard.Role = role;
